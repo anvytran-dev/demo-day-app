3854149c3d028a211f119ab53033cd4bda484d8f
Anvy Tran <anvytran857@gmail.com>
2021-03-28 22:51:01 -0400

    demo-day: first commit, building first draft

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://anvytran:matcha@anvyrc.kuion.mongodb.net/signUpCube?retryWrites=true&w=majority', 
+    'dbName': 'signUpCube'
+};
